dd8b4a59e3752f7b7c050791e237a1f4c394d194
cpastorelli <cpastorelli@arcanabrewing.com>
2022-03-11 14:13:02 -0800

    Finished setting up boilerplate server.js file

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const apiRoutes = require('./routes');
+const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+app.use(apiRoutes);
 
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`Listening on port ${PORT}`);
+    });
+});
