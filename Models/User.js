6d01481b58e346630a34819c1bcc370789e5c017
cpastorelli <cpastorelli@arcanabrewing.com>
2022-03-11 15:21:12 -0800

    created outline of User Model

---
+++ Models/User.js
@@ -0,0 +1,12 @@
+const {Schema, model} = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {},
+        email: {},
+        thoughts: [],
+        friends: [],
+    }
+);
+
+module.exports = userSchema;
