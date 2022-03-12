0ffe81b0ea99de5c29d3ebe0ba75de27b9e35c82
cpastorelli <cpastorelli@arcanabrewing.com>
2022-03-11 20:13:49 -0800

    Added comments to Thought Controller

---
+++ controller/thoughts-controller.js
@@ -0,0 +1,20 @@
+const { Thought, User } = require('../models');
+
+const thoughtController = {
+    // GET all thougths
+    getAllThoughts() {},
+    // GET One Thought
+    getOneThought() {},
+    // Create a New Thought
+    makeNewThought() {},
+    // Update a Thought
+    updateThought() {},
+    // Delete a Thought
+    forgetThought() {},
+    // Create a new Reaction
+    newReaction() {},
+    // Remove a Reaction
+    removeReaction() {},
+};
+
+module.exports = thoughtController;
