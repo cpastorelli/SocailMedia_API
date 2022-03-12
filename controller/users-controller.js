e6741e6b18784357de40a9ee7c170c71dd92bce5
cpastorelli <cpastorelli@arcanabrewing.com>
2022-03-11 16:03:05 -0800

    created outline of user and thougth controller functions- Need to fill them in

--- controller/users-controller.js
+++ controller/users-controller.js
@@ -1,7 +1,13 @@
 const { User, Thought } = require('../models');
 
 const userController = {
-
-}
+    getAllUsers() {},
+    getOneUser() {},
+    makeNewUser() {},
+    updateUser() {},
+    removeUser() {},
+    addFriend() {},
+    removeFriend() {},
+};
 
 module.exports = userController;
