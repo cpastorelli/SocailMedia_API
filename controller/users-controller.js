ac8e00ae583cb5a3979d0998a7862815e4a28749
cpastorelli <cpastorelli@arcanabrewing.com>
2022-03-11 20:17:05 -0800

    Added comments to User Controller

--- controller/users-controller.js
+++ controller/users-controller.js
@@ -1,12 +1,19 @@
 const { User, Thought } = require('../models');
 
 const userController = {
+    // GET all users
     getAllUsers() {},
+    // GET One User
     getOneUser() {},
+    // Create a new User
     makeNewUser() {},
+    // Update a new User by ID
     updateUser() {},
+    // Remove a User by ID
     removeUser() {},
+    // Add a new Friend
     addFriend() {},
+    // Remove a Friend
     removeFriend() {},
 };
 
