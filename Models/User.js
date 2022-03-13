86df586f4208a1092880274f2907fd39360c8037
Caroline Pastorelli <cpastorelli@arcanabrewing.com>
2022-03-13 14:17:14 -0700

    filled in User Models section

--- Models/User.js
+++ Models/User.js
@@ -3,11 +3,41 @@ const {Schema, model} = require('mongoose');
 // Setting up User Schema - will have thoughts, and friends
 const userSchema = new Schema(
     {
-        username: {},
-        email: {},
-        thoughts: [],
-        friends: [],
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/.+@+\..+/, 'Must be a valid e-mail address!'],
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
     }
 );
+
+userSchema.virtual('friendsCount').get(function () {
+    return this.friends.length;
+});
+const User = model('User', userSchema);
 
 module.exports = userSchema;
